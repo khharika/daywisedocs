bf2541bb4eb1bb19cad34620734a40c06aa95e0a
harika <kantipudiharika98@gmail.com>
2025-08-22 11:18:34 +0530

    DL

---
+++ IdeaProjects/SORT/src/Day26/DifficultLevel.java
@@ -0,0 +1,8 @@
+package Day26;
+public class DifficultLevel implements DifficultyLevel {
+    @Override
+    public void play() {
+        System.out.println("This is a Difficult level game");
+    }
+}
+
