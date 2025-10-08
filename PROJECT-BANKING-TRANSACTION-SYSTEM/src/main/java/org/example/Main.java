6829ae01fdd82353e9c6a98bd4c4465607da049e
khharika <kantipudiharika98@gmail.com>
2025-10-08 13:14:39 +0530

    Delete PROJECT-BANKING-TRANSACTION-SYSTEM/src directory

--- PROJECT-BANKING-TRANSACTION-SYSTEM/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        BankingSystem bankingSystem = new BankingSystem();
-        bankingSystem.start();
-    }
-}
